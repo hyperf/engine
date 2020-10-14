24a3a7a7054c139cce93fdd8b4bd05b961dc2a76
李铭昕 <715557344@qq.com>
2020-10-14 14:17:58 +0800

    first commit

---
+++ src/Coroutine.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Engine;
+
+use Hyperf\Engine\Contract\CoroutineInterface;
+use Hyperf\Engine\Exception\RuntimeException;
+use Swoole\Coroutine as SwooleCo;
+
+class Coroutine implements CoroutineInterface
+{
+    /**
+     * @var callable
+     */
+    private $callable;
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    public function __construct(callable $callable, int $stackPageSize = 0, int $stackSize = 0)
+    {
+        $this->callable = $callable;
+    }
+
+    public static function run(callable $callable, ...$data)
+    {
+        $coroutine = new static($callable);
+        $coroutine->execute(...$data);
+        return $coroutine;
+    }
+
+    public function execute(...$data)
+    {
+        $this->id = SwooleCo::create($this->callable, ...$data);
+    }
+
+    public function getId()
+    {
+        if (is_null($this->id)) {
+            throw new RuntimeException('Coroutine was not be executed.');
+        }
+        return $this->id;
+    }
+
+    public static function id()
+    {
+        return SwooleCo::getCid();
+    }
+}
