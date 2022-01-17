900fddd493bc4f5b4a0a2524f9cba7253d4af5f4
李铭昕 <715557344@qq.com>
2022-01-17 12:02:04 +0800

    Only Support Swoole >= 5.0

--- src/Channel.php
+++ src/Channel.php
@@ -14,129 +14,68 @@ namespace Hyperf\Engine;
 use Hyperf\Engine\Contract\ChannelInterface;
 use Hyperf\Engine\Exception\RuntimeException;
 
-if (PHP_VERSION_ID > 80000 && SWOOLE_VERSION_ID >= 40900) {
-    class Channel extends \Swoole\Coroutine\Channel implements ChannelInterface
+class Channel extends \Swoole\Coroutine\Channel implements ChannelInterface
+{
+    protected bool $closed = false;
+
+    public function push(mixed $data, float $timeout = -1): bool
     {
-        protected bool $closed = false;
-
-        public function push(mixed $data, float $timeout = -1): bool
-        {
-            return parent::push($data, $timeout);
-        }
-
-        public function pop(float $timeout = -1): mixed
-        {
-            return parent::pop($timeout);
-        }
-
-        public function getCapacity(): int
-        {
-            return $this->capacity;
-        }
-
-        public function getLength(): int
-        {
-            return $this->length();
-        }
-
-        public function isAvailable(): bool
-        {
-            return ! $this->isClosing();
-        }
-
-        public function close(): bool
-        {
-            $this->closed = true;
-            return parent::close();
-        }
-
-        public function hasProducers(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
-
-        public function hasConsumers(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
-
-        public function isReadable(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
-
-        public function isWritable(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
-
-        public function isClosing(): bool
-        {
-            return $this->closed || $this->errCode === SWOOLE_CHANNEL_CLOSED;
-        }
-
-        public function isTimeout(): bool
-        {
-            return ! $this->closed && $this->errCode === SWOOLE_CHANNEL_TIMEOUT;
-        }
+        return parent::push($data, $timeout);
     }
-} else {
-    class Channel extends \Swoole\Coroutine\Channel implements ChannelInterface
+
+    public function pop(float $timeout = -1): mixed
     {
-        /**
-         * @var bool
-         */
-        protected $closed = false;
+        return parent::pop($timeout);
+    }
 
-        public function getCapacity(): int
-        {
-            return $this->capacity;
-        }
+    public function getCapacity(): int
+    {
+        return $this->capacity;
+    }
 
-        public function getLength(): int
-        {
-            return $this->length();
-        }
+    public function getLength(): int
+    {
+        return $this->length();
+    }
 
-        public function isAvailable(): bool
-        {
-            return ! $this->isClosing();
-        }
+    public function isAvailable(): bool
+    {
+        return ! $this->isClosing();
+    }
 
-        public function close(): bool
-        {
-            $this->closed = true;
-            return parent::close();
-        }
+    public function close(): bool
+    {
+        $this->closed = true;
+        return parent::close();
+    }
 
-        public function hasProducers(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
+    public function hasProducers(): bool
+    {
+        throw new RuntimeException('Not supported.');
+    }
 
-        public function hasConsumers(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
+    public function hasConsumers(): bool
+    {
+        throw new RuntimeException('Not supported.');
+    }
 
-        public function isReadable(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
+    public function isReadable(): bool
+    {
+        throw new RuntimeException('Not supported.');
+    }
 
-        public function isWritable(): bool
-        {
-            throw new RuntimeException('Not supported.');
-        }
+    public function isWritable(): bool
+    {
+        throw new RuntimeException('Not supported.');
+    }
 
-        public function isClosing(): bool
-        {
-            return $this->closed || $this->errCode === SWOOLE_CHANNEL_CLOSED;
-        }
+    public function isClosing(): bool
+    {
+        return $this->closed || $this->errCode === SWOOLE_CHANNEL_CLOSED;
+    }
 
-        public function isTimeout(): bool
-        {
-            return ! $this->closed && $this->errCode === SWOOLE_CHANNEL_TIMEOUT;
-        }
+    public function isTimeout(): bool
+    {
+        return ! $this->closed && $this->errCode === SWOOLE_CHANNEL_TIMEOUT;
     }
 }
