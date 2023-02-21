7d565dd1a4d1ed653492045baa70ffc77041943f
李铭昕 <715557344@qq.com>
2023-02-21 13:48:59 +0800

    Update SocketType.php

--- src/Constant/SocketType.php
+++ src/Constant/SocketType.php
@@ -13,15 +13,15 @@ namespace Hyperf\Engine\Constant;
 
 class SocketType
 {
-    public const SOCK_TCP = SWOOLE_SOCK_TCP;
+    public const TCP = SWOOLE_SOCK_TCP;
 
-    public const SOCK_TCP6 = SWOOLE_SOCK_TCP6;
+    public const TCP6 = SWOOLE_SOCK_TCP6;
 
-    public const SOCK_UDP = SWOOLE_SOCK_UDP;
+    public const UDP = SWOOLE_SOCK_UDP;
 
-    public const SOCK_UDP6 = SWOOLE_SOCK_UDP6;
+    public const UDP6 = SWOOLE_SOCK_UDP6;
 
-    public const SOCK_UNIX_STREAM = SWOOLE_SOCK_UNIX_STREAM;
+    public const UNIX_STREAM = SWOOLE_SOCK_UNIX_STREAM;
 
-    public const SOCK_UNIX_DGRAM = SWOOLE_SOCK_UNIX_DGRAM;
+    public const UNIX_DGRAM = SWOOLE_SOCK_UNIX_DGRAM;
 }
