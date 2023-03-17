2fcaf63de5f89bfa700c0a0324c8b6dc5099dfcb
李铭昕 <715557344@qq.com>
2023-03-17 13:30:11 +0800

    Support HTTP2 Client.

---
+++ tests/Cases/Http2ClientTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Cases;
+
+use Hyperf\Engine\Http\V2\Client;
+use Hyperf\Engine\Http\V2\Request;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class Http2ClientTest extends AbstractTestCase
+{
+    /**
+     * @group Server
+     */
+    public function testHttp2ServerReceived()
+    {
+        $this->runInCoroutine(function () {
+            $client = new Client('127.0.0.1', 9501);
+            $client->send(new Request('/'));
+            $response = $client->recv(1);
+            $this->assertSame('Hello World.', $response->getBody());
+
+            $client->send(new Request('/header'));
+            $response = $client->recv(1);
+            $id = $response->getHeaders()['x-id'];
+            $this->assertSame($id, $response->getBody());
+
+            $client->send(new Request('/not-found'));
+            $response = $client->recv(1);
+            $this->assertSame(404, $response->getStatusCode());
+        });
+    }
+}
