91889620a4ab675b87e211fd672ea8de92dc64e6
李铭昕 <715557344@qq.com>
2023-03-17 15:43:12 +0800

    Added HTTP2 Client.

---
+++ src/Contract/Http/V2/ClientInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Engine\Contract\Http\V2;
+
+interface ClientInterface
+{
+    public function set(array $settings): bool;
+
+    /**
+     * @return int StreamID
+     */
+    public function send(RequestInterface $request): int;
+
+    public function recv(float $timeout = 0): ResponseInterface;
+
+    public function ping(): bool;
+
+    public function close(): bool;
+
+    public function isConnected(): bool;
+}
