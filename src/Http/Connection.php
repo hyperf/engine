c4eed24bd48a87d31cec102a08630fd3dd4ec5b6
Deeka Wong <huangdijia@gmail.com>
2023-03-24 18:25:16 +0800

    Adds HTTP Connection (#14)

---
+++ src/Http/Connection.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Engine\Http;
+
+use Hyperf\Engine\Contract\Http\ConnectionInterface;
+use Swoole\Http\Response;
+
+class Connection implements ConnectionInterface
+{
+    public function __construct(protected Response $response)
+    {
+    }
+
+    public function write(string $data): bool
+    {
+        return $this->response->write($data);
+    }
+
+    /**
+     * @return Response
+     */
+    public function getSocket(): mixed
+    {
+        return $this->response;
+    }
+
+    public function end(?string $content = null): bool
+    {
+        return $this->response->end($content);
+    }
+}
