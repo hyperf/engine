8465538aa06d29d4a093fdab3e67303a153bad68
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-03-24 17:35:43 +0800

    [1.x] Adds HTTP Connection

---
+++ src/Http/Connection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Hyperf\Engine\Http;
+
+use Hyperf\Engine\Contract\Http\ConnectionInterface;
+use Hyperf\Engine\Contract\Http\Chunkable;
+
+use Swoole\Http\Response;
+
+class Connection implements ConnectionInterface, Chunkable
+{
+    public function __construct(protected Response $response)
+    {
+    }
+
+    public function write(string $data): bool
+    {
+        return $this->response->write($data);
+    }
+
+    /**
+     * @return Response 
+     */
+    public function getSocket(): mixed
+    {
+        return $this->response;
+    }
+
+    public function end(): void
+    {
+        $this->response->end();
+    }
+}
