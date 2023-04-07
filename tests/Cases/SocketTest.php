450c8af828cbe3a90d2a8994d44728ebe8405b4e
李铭昕 <715557344@qq.com>
2023-04-07 13:21:17 +0800

    Added test cases about safe client closed.

--- tests/Cases/SocketTest.php
+++ tests/Cases/SocketTest.php
@@ -169,4 +169,67 @@ class SocketTest extends AbstractTestCase
             $server->shutdown();
         });
     }
+
+    public function testSafeSocketBroken()
+    {
+        $this->runInCoroutine(function () {
+            $server = new Server('0.0.0.0', 9506);
+            $p = function (string $data): string {
+                return pack('N', strlen($data)) . $data;
+            };
+            go(function () use ($server, $p) {
+                $server->set([
+                    'open_length_check' => true,
+                    'package_max_length' => 1024 * 1024 * 2,
+                    'package_length_type' => 'N',
+                    'package_length_offset' => 0,
+                    'package_body_offset' => 4,
+                ]);
+                $server->handle(function (Server\Connection $connection) use ($p) {
+                    $socket = new SafeSocket($connection->exportSocket(), 65535);
+                    while (true) {
+                        try {
+                            $body = $socket->recvPacket();
+                            if (empty($body)) {
+                                break;
+                            }
+                            go(function () use ($socket, $body, $p) {
+                                $body = substr($body, 4);
+                                if ($body === 'ping') {
+                                    $socket->sendAll($p('pong'));
+                                } else {
+                                    $socket->sendAll($p($body));
+                                }
+                            });
+                        } catch (Throwable $exception) {
+                            $this->assertInstanceOf(SocketClosedException::class, $exception);
+                            break;
+                        }
+                    }
+                });
+                $server->start();
+            });
+
+            sleep(1);
+
+            $socket = (new Socket\SocketFactory())->make(new Socket\SocketOption('127.0.0.1', 9506, protocol: [
+                'open_length_check' => true,
+                'package_max_length' => 1024 * 1024 * 2,
+                'package_length_type' => 'N',
+                'package_length_offset' => 0,
+                'package_body_offset' => 4,
+            ]));
+
+            $socket->sendAll($p(str_repeat('s', 10240)), 1);
+            $socket->recvPacket(1);
+            $socket->sendAll($p(str_repeat('s', 10240)), 1);
+            $socket->recvPacket(1);
+
+            $socket->close();
+
+            sleep(1);
+
+            $server->shutdown();
+        });
+    }
 }
