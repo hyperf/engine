3805a9d3b02a60f5b508dfffab7c63edf1b88cee
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-04-12 13:41:12 +0800

    Add tests

---
+++ tests/Cases/BarrierTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HyperfTest\Cases;
+
+use Hyperf\Engine\Barrier;
+use Hyperf\Engine\Coroutine;
+
+class BarrierTest extends AbstractTestCase
+{
+    public function testBarrier()
+    {
+        $this->runInCoroutine(function () {
+            $barrier = Barrier::create();
+            $N = 10;
+            $count = 0;
+            for ($i = 0; $i < $N; $i++) {
+                Coroutine::create(function () use ($barrier, &$count) {
+                    usleep(2000);
+                    $count++;
+                });
+            }
+            Barrier::wait($barrier);
+            $this->assertSame($N, $count);
+        });
+    }
+}
