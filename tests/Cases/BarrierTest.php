773d9121ec6239875b206754cb4b4be49dbb22f8
李铭昕 <715557344@qq.com>
2025-04-13 22:53:16 +0800

    Format

--- tests/Cases/BarrierTest.php
+++ tests/Cases/BarrierTest.php
@@ -1,10 +1,24 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
 namespace HyperfTest\Cases;
 
 use Hyperf\Engine\Barrier;
 use Hyperf\Engine\Coroutine;
 
+/**
+ * @internal
+ * @coversNothing
+ */
 class BarrierTest extends AbstractTestCase
 {
     public function testBarrier()
@@ -13,10 +27,10 @@ class BarrierTest extends AbstractTestCase
             $barrier = Barrier::create();
             $N = 10;
             $count = 0;
-            for ($i = 0; $i < $N; $i++) {
-                Coroutine::create(function () use ($barrier, &$count) {
+            for ($i = 0; $i < $N; ++$i) {
+                Coroutine::create(function () use (&$count) {
                     usleep(2000);
-                    $count++;
+                    ++$count;
                 });
             }
             Barrier::wait($barrier);
